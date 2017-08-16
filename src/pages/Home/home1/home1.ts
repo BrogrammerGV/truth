4db923aaa470c57116efd932d71458eaa912928f
Travis Nelsen <TNelsen@Homesteaderslife.com>
2017-08-16 12:53:25 -0500

    Register page created

--- src/pages/Home/home1/home1.ts
+++ src/pages/Home/home1/home1.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { LoginPage } from '../../../pages/login/login';
-
+declare let registerCognito: any;
 /**
  * Generated class for the Home1Page page.
  *
@@ -18,6 +18,8 @@ export class Home1Page {
   public buttonClicked: boolean = false;
   public searchingClicked: boolean = false;
   public planningClicked: boolean = false;
+  public buttonText: string = "Login";
+  shouldHeight = document.body.clientHeight + 'px';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -34,6 +36,7 @@ export class Home1Page {
     this.buttonClicked = true;
     this.planningClicked = true;
     this.searchingClicked = false;
+    this.buttonText = "Next";
   }
 
   searching(){
@@ -43,7 +46,25 @@ export class Home1Page {
   }
   
   login(){
-    this.navCtrl.push(LoginPage);
+    if(this.buttonText == "Login"){
+      this.navCtrl.push(LoginPage);
+    }else{
+      registerCognito({
+        ClientId: 'mvt58lr4h6sc8rrs1t62n9h6o', /* required */
+        Password: 'TestPassword1', /* required */
+        Username: 'TestUser', /* required */
+        UserAttributes: [
+          {
+            Name: 'family_name', /* required */
+            Value: 'TestUser'
+          },
+          {
+            Name: 'email', /* required */
+            Value: 'test@test.com'
+          },
+          /* more items */
+        ]
+      });
+    }
   }
-
 }
