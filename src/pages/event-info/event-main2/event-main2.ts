6aa19658cfaa056cb2046b03c88abaae922dee62
iOS Dev <iosdev@Programmer-MBP.local>
2017-08-29 09:50:32 -0500

    EAD-190 Add item UIS start

--- src/pages/event-info/event-main2/event-main2.ts
+++ src/pages/event-info/event-main2/event-main2.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-import {Storage} from '@ionic/storage';
+import { Storage } from '@ionic/storage';
 import { EventInfoOnePage } from '../event-info-one/event-info-one';
 import { EventMainPage } from '../event-main/event-main';
 import { EventMainPage3 } from '../event-main3/event-main3';
@@ -28,33 +28,35 @@ declare let performMetaGet: any;
 export class EventMainPage2 {
 
 
-//Dynamic Variables 
-//Variables for Data Load
-nameToUse: string;
-eventTime: string;
-eventDate: string;
-eventMonth: string;
-funeralHome: string;
-firstName: string;
+  //Dynamic Variables 
+  //Variables for Data Load
+  nameToUse: string;
+  eventTime: string;
+  eventDate: string;
+  eventMonth: string;
+  funeralHome: string;
+  firstName: string;
+  careCategory: string;
 
 
-//Bool Checks
-showEditScreen : boolean = false;
-hasSeenCare : boolean = false;
-careButtonText : string = "Get Started";
+  //Bool Checks
+  showEditScreen: boolean = false;
+  showAddItem: boolean = false;
+  hasSeenCare: boolean = false;
+  careButtonText: string = "Get Started";
 
-//Vars
-helperText: string;
-helperText2: string;
-greetingText: string;
-messageText: string;
+  //Vars
+  helperText: string;
+  helperText2: string;
+  greetingText: string;
+  messageText: string;
 
 
 
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams, 
-  private storage: Storage, 
-  public modalCtrl: ModalController
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage,
+    public modalCtrl: ModalController
   ) {
   }
 
@@ -63,18 +65,16 @@ messageText: string;
 
 
 
-ionViewCanEnter()
-  {
+  ionViewCanEnter() {
     this.storage.get('hasSeenCare').then((val) => {
-                    if(val == "Y")
-                      {
-                           this.hasSeenCare = true;
-                          this.careButtonText = "Add Care Item";
+      if (val == "Y") {
+        this.hasSeenCare = true;
+        this.careButtonText = "Add Care Item";
 
-                      }
-                         
-                        });
-        
+      }
+
+    });
+
   }
 
 
@@ -83,116 +83,121 @@ ionViewCanEnter()
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventMainPage');
     this.storage.get('guid').then((val) => {
-    console.log('Guid:', val);
-    this.doSearch(val);
-  });
- 
-   
+      console.log('Guid:', val);
+      this.doSearch(val);
+    });
+
+
   }
 
 
 
- doSearch(guid:string){
-   
-    performMetaGet({"eventID": guid
-    }).then(function(data: any){
- 
+  doSearch(guid: string) {
+
+    performMetaGet({
+      "eventID": guid
+    }).then(function (data: any) {
+
       this.logItem(data);
 
     }.bind(this));
 
   }
 
-logItem(ref: any){
-  
+  logItem(ref: any) {
+
     var x = JSON.parse(ref.Payload)
-console.log(x.Item.firstName);
-   this.nameToUse = x.Item.firstName.S + " " + x.Item.lastName.S;
-   this.eventDate = x.Item.eventDate.S;
-   this.eventTime = x.Item.eventTime.S;
-   this.funeralHome = x.Item.funeralHome.S;
-   this.eventMonth = x.Item.eventMonth.S;
-   this.firstName = x.Item.firstName.S;
+    console.log(x.Item.firstName);
+    this.nameToUse = x.Item.firstName.S + " " + x.Item.lastName.S;
+    this.eventDate = x.Item.eventDate.S;
+    this.eventTime = x.Item.eventTime.S;
+    this.funeralHome = x.Item.funeralHome.S;
+    this.eventMonth = x.Item.eventMonth.S;
+    this.firstName = x.Item.firstName.S;
 
-   //Setting Variable Texr
-        this.helperText = "A Message from "+ this.firstName + "'s Family";
-        this.helperText2 = "Thank you for supporting our family during this difficult time We appreciate your condolences and invite you to join us as we celelbrate "  + this.firstName + ".";
-        this.messageText = this.helperText2;
-        this.greetingText = this.helperText;
- 
+    //Setting Variable Texr
+    this.helperText = "A Message from " + this.firstName + "'s Family";
+    this.helperText2 = "Thank you for supporting our family during this difficult time We appreciate your condolences and invite you to join us as we celelbrate " + this.firstName + ".";
+    this.messageText = this.helperText2;
+    this.greetingText = this.helperText;
+
   }
-  goNext()
-  {
-this.navCtrl.push(EventInfoOnePage)
+  goNext() {
+    this.navCtrl.push(EventInfoOnePage)
   }
 
 
-swapScreen()
-{
+  swapScreen() {
 
-this.showEditScreen = true;
+    this.showEditScreen = true;
 
-}
+  }
 
-removeEditScreen()
-{
-  this.showEditScreen=false;
-}
+  removeEditScreen() {
+    this.showEditScreen = false;
+  }
 
-saveEditText()
-{
-  if(this.greetingText)
-  this.helperText = this.greetingText;
-  if(this.helperText)
-  this.helperText2 = this.messageText
-  
+  saveEditText() {
+    if (this.greetingText)
+      this.helperText = this.greetingText;
+    if (this.helperText)
+      this.helperText2 = this.messageText
 
-  this.showEditScreen = false;
-}
 
-goToEvents()
-{
-  this.navCtrl.setRoot(EventMainPage);
-}
+    this.showEditScreen = false;
+  }
 
-goToFeed()
-{
-  this.navCtrl.setRoot(EventMainPage3);
-}
+  goToEvents() {
+    this.navCtrl.setRoot(EventMainPage);
+  }
+
+  goToFeed() {
+    this.navCtrl.setRoot(EventMainPage3);
+  }
 
 
 
 
-getStarted()
-{
+  getStarted() {
 
-  if(this.careButtonText == "Get Started")
-    {
+    if (this.careButtonText == "Get Started") {
 
-this.storage.set('hasSeenCare', "Y");
- this.hasSeenCare = true;
- this.careButtonText = "Add Care Item"
- this.openCareModal();
+      this.storage.set('hasSeenCare', "Y");
+      this.hasSeenCare = true;
+      this.careButtonText = "Add Care Item"
+      this.openCareModal();
 
 
     }
+    if(this.careButtonText=="Add Care Item")
+    {
+       this.showAddItem = true;
+       this.careButtonText = "Add Item"
+    }
     else {
-      this.storage.set('hasSeenCare', "N");
+
+      if(!this.careCategory){
+          this.storage.set('hasSeenCare', "N");
       this.careButtonText = "Get Started"
       this.hasSeenCare = false;
     }
+    else 
+    {
+      console.log(this.careCategory)
+    }
+    
+    }
 
 
-}
+  }
 
 
 
 
-        openCareModal()
-            {
-                    let myModal = this.modalCtrl.create(CareModalPage);
-                                
-                  myModal.present();
-            }
+  openCareModal() {
+    let myModal = this.modalCtrl.create(CareModalPage);
+
+    myModal.present();
+  }
 
 }
